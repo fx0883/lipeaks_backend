f486f6da78352eef010bc73b1d0a68c992ffa7b6
fx0883 <jackfeng8123@gmail.com>
2025-11-27 22:22:54 +0800

    123
    13

--- core/wsgi.py
+++ core/wsgi.py
@@ -18,7 +18,8 @@ os.environ.setdefault('PYTHONIOENCODING', 'utf-8')
 
 import pymysql
 
-from django.core.wsgi import get_wsgi_application
+# 必须在导入 Django 之前设置
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 # 设置pymysql的连接超时和尝试次数
 pymysql.connect_timeout = 30
@@ -27,6 +28,5 @@ pymysql.reconnect_attempt = 3
 # 使用pymysql代替mysqlclient
 pymysql.install_as_MySQLdb()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
