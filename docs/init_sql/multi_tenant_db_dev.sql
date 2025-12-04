f668e934341351955a06d476aa4249898a428ef2
fx0883 <jackfeng8123@gmail.com>
2025-12-04 20:09:42 +0800

    Update multi_tenant_db_dev.sql

--- docs/init_sql/multi_tenant_db_dev.sql
+++ docs/init_sql/multi_tenant_db_dev.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c0cb5acba3c206fb2cbee369d7bb8033992dd35efc7f53a27e7805c92b16064
-size 135191733
+oid sha256:48c2eb1890762e9a34c4e159df2fa3aa0cee49065435a2c01628563e58cc7d5d
+size 135384552
